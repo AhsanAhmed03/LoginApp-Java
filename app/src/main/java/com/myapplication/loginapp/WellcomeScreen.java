47af86cbf8498a4d5185d9053bda7ab6bf8fdbdf
Ahsan Ahmed <aahsanaahmed26@gmail.com>
2023-05-26 17:56:16 +0500

    Simple Login Page

---
+++ app/src/main/java/com/myapplication/loginapp/WellcomeScreen.java
@@ -0,0 +1,14 @@
+package com.myapplication.loginapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WellcomeScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wellcome_screen);
+    }
+}
